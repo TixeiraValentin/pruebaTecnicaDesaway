66def022e73a9eb35a559d088d2e6a814a5c9416
TixeiraValentin <valen.tixeira@gmail.com>
2025-08-20 20:20:30 -0300

    feat: add Colors theme file with primary and surface color definitions

---
+++ src/presentation/theme/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+	primary: '#8C70D4',
+	surface: '#FCFBFF',
+	textPrimary: '#4D406E',
+	textOnPrimary: '#FFFFFF',
+	border: '#E3DDF7',
+} as const;
+
+export type AppColors = typeof Colors;
+
+
